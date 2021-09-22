7e50bab29a36e1c519f13e0062b917c1dedf9a6e
HoseaSims <hoseasims21@gmail.com>
2021-09-22 16:18:53 -0600

    starting styling joblist

--- blocks/job-list/job-list.js
+++ blocks/job-list/job-list.js
@@ -3,5 +3,12 @@
  * @param {HTMLElement} $block 
  */
 export default function decorate($block) {
-    
+    const $boards = $block.querySelectorAll(":scope > div");
+    for(const board of $boards) {
+      board.classList.add("joblisting");
+      board.querySelector("div:nth-child(1)").classList.add("jobtitle");
+      board.querySelector("div:nth-child(2)").classList.add("position");
+      board.querySelector("p:nth-child(3)").classList.add("experience");
+      board.querySelector("div:nth-child(3)").classList.add("location");
+    }
 }
