57170bb8d5cb9218b612b2a8ebe4cf2ba9b50707
Logan Kovash <logan.kovash@busyhuman.io>
2021-09-22 14:26:45 -0600

    updated hero

--- blocks/hero-carousel/hero-carousel.js
+++ blocks/hero-carousel/hero-carousel.js
@@ -9,6 +9,6 @@ export default function decorate($block) {
         slide.querySelector(":nth-child(1)").classList.add("image");
         slide.querySelector(":nth-child(2) p:first-child").classList.add("tag");
         slide.querySelector(":nth-child(2) h2").classList.add("title");
-        slide.querySelector(":nth-child(3) p:last-child").classList.add("author");
+        slide.querySelector(":nth-child(2) p:last-child").classList.add("author");
     }
 }
