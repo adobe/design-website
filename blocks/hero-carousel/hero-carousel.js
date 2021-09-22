09df985eefee83876211d4e28a6eafaaded0660d
Jonathan Oaklyn <jonathan.revell.hopkins@gmail.com>
2021-09-22 13:49:47 -0600

    Hero carousel decorator

---
+++ blocks/hero-carousel/hero-carousel.js
@@ -0,0 +1,14 @@
+/**
+ *
+ * @param {HTMLElement} $block
+ */
+export default function decorate($block) {
+    var $slides = $block.querySelectorAll(":scope > div");
+    for(const slide of $slides) {
+        slide.classList.add("slide");
+        slide.querySelector(":nth-child(1)").classList.add("image");
+        slide.querySelector(":nth-child(2) p:first-child").classList.add("tag");
+        slide.querySelector(":nth-child(2) h2").classList.add("title");
+        slide.querySelector(":nth-child(3) p:last-child").classList.add("author");
+    }
+}
