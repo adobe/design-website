46172ee7d268bf3d334f035f853917b9e6702759
Logan Kovash <logan.kovash@busyhuman.io>
2021-09-22 16:58:18 -0600

    people

--- blocks/people/people.js
+++ blocks/people/people.js
@@ -2,11 +2,12 @@
 export default function decorate($block) {
    const $people = $block.querySelectorAll(':scope > div');
    for(const person of $people) {
-       person.classList.add('person');
-       person.querySelector("div:nth-child(1)").classList.add("image");
+       person.classList.add("person");
+       person.querySelector("div:nth-child(1)").classList.add("content");
        person.querySelector("p:nth-child(1)").classList.add("tag");
-       person.querySelector("div:nth-child(3)").classList.add("content");
-       person.querySelector("div:nth-child(4)").classList.add("name");
-       person.querySelector("div:nth-child(5)").classList.add("title");
+       person.querySelector("p:nth-child(2)").classList.add("pad");
+       person.querySelector("p:nth-child(2) > picture > img").classList.add("image");
+       person.querySelector("div:nth-child(2)").classList.add("name");
+       person.querySelector("div:nth-child(3)").classList.add("title");
    }
 }
