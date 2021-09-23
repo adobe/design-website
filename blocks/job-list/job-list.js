3b0fdef6492026370ee22b01ff94e857e70be20f
Logan Kovash <logan.kovash@busyhuman.io>
2021-09-23 10:40:00 -0600

    updated jobs

--- blocks/job-list/job-list.js
+++ blocks/job-list/job-list.js
@@ -7,8 +7,8 @@ export default function decorate($block) {
     for(const board of $boards) {
       board.classList.add("job-listing");
       board.querySelector("div:first-child h3").classList.add("job-title");
-      board.querySelector("div:first-child p").classList.add("position");
-      board.querySelector("div:nth-child(2) p:nth-child(1)").classList.add("experience");
-      board.querySelector("div:nth-child(2) p:nth-child(2)").classList.add("location");
+      board.querySelector("div p:nth-child(2)").classList.add("experience");
+      board.querySelector("div p:nth-child(3)").classList.add("location");
+      board.querySelector("div p:nth-child(4)").classList.add("position");
     }
 }
