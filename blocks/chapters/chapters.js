e67afc0817822e28dba56f74dd3fe60130f895a4
Jonathan Oaklyn <jonathan.revell.hopkins@gmail.com>
2021-09-24 12:29:04 -0600

    Expose page properties globally, chapter decorator

---
+++ blocks/chapters/chapters.js
@@ -0,0 +1,15 @@
+/**
+ * 
+ * @param {HTMLElement} $block 
+ */
+export default function decorate($block) {
+    const $chapters = $block.querySelectorAll(":scope > div");
+    $chapters.forEach(($chapter) => {
+        $chapter.classList.add("chapter");
+        const $heading = $chapter.querySelector(":scope > div:first-child");
+        const $content = $chapter.querySelector(":scope > div:last-child");
+
+        $heading.classList.add("chapter-heading");
+        $content.classList.add("chapter-content");
+    });
+}
