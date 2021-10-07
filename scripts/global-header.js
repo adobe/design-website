377b10d694d48a2de6e2212dd0c0274936ecd2c1
Jonathan Oaklyn <jonathan.revell.hopkins@gmail.com>
2021-10-07 13:28:20 -0600

    Link the adobe design logo to home

--- scripts/global-header.js
+++ scripts/global-header.js
@@ -5,5 +5,8 @@ export default function decorate(win = window) {
   if ($header) {
     $header.textContent = "Hello World";
     $header.innerHTML = '<div> <div class="logo"> <picture> <source media=\"(max-width: 400px)\" srcset=\"./media_1d3601e007f7c9b5cb83303a390ca0e7fec040eef.png?width=750&format=webply&optimize=medium\"> <img src=\"./media_1d3601e007f7c9b5cb83303a390ca0e7fec040eef.png?width=2000&format=webply&optimize=medium\" alt=\"\" loading=\"eager\"> </picture> </div> </div> <div class="tagline"> <p>Stories from the team designing Creative Cloud, Document Cloud, and Experience Cloud.</p> </div> <nav> <a>Team</a> <a>Stories</a> <a>Tools</a> <a>Jobs</a> </div>';
+    $header.querySelector("picture").addEventListener("click", () => {
+      location.pathname = "/";
+    }); 
   }
 }
