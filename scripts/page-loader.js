4a393f511b3febf0de77a193f71e135a897cb5b3
Jonathan Oaklyn <jonathan.revell.hopkins@gmail.com>
2021-10-07 09:27:15 -0600

    Working on loader

---
+++ scripts/page-loader.js
@@ -0,0 +1,19 @@
+const PID = "page-loading-placeholder";
+
+export function setPageLoading() {
+    const $placeholder = document.createElement("div");
+    $placeholder.id = PID;
+    $placeholder.appendChild( document.createElement("div") );
+    $placeholder.appendChild( document.createElement("div") );
+    document.body.appendChild($placeholder);
+}
+
+export function pageDoneLoading() {
+    setTimeout(() => {
+        document.body.classList.add("loaded");
+        const $placeholder = document.querySelector(`#${PID}`);
+        if ($placeholder) {
+            $placeholder.remove();
+        }
+    }, 5000);
+}
