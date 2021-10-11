ac0319a7d95d3553a67958266f0b03968fdaf455
Connor O'Leary <connorroleary@gmail.com>
2021-10-11 10:19:56 -0600

    Updated footer to align with page gutter

--- scripts/global-footer.js
+++ scripts/global-footer.js
@@ -3,6 +3,6 @@ export default function decorate(win = window) {
     const $footer = doc.querySelector("body > footer");
     $footer.id = "global-footer";
     if ($footer) {
-      $footer.innerHTML = '<img class="source" src="/resources/adobe-design.png"> <nav> <a href="">Behance</a> <a href="">Instagram</a> <a href="">Twitter</a> </nav> <nav> <a href="">Adobe Jobs</a> <a href="">Adobe Stories</a> </nav> <nav> <p>Copyright © 2021 Adobe. All rights reserved.</p> <p>/</p> <a href="">Privacy</a> <p>/</p> <a href="">Terms of Use</a> <p>/</p> <a href="">Cookies</a> <p>/</p> <a href="">Do not sell my personal information</a> </nav>';
+      $footer.innerHTML = '<div class="footer-content"><img class="source" src="/resources/adobe-design.png"> <nav> <a href="">Behance</a> <a href="">Instagram</a> <a href="">Twitter</a> </nav> <nav> <a href="">Adobe Jobs</a> <a href="">Adobe Stories</a> </nav> <nav> <p>Copyright © 2021 Adobe. All rights reserved.</p> <p>/</p> <a href="">Privacy</a> <p>/</p> <a href="">Terms of Use</a> <p>/</p> <a href="">Cookies</a> <p>/</p> <a href="">Do not sell my personal information</a> </nav> </div>';
     }
 }
