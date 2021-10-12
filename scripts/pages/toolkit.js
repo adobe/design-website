c522f4586ee48268b0b932eec1f30e8eaaddb2d2
wmichaelbischoff <wmichaelbischoff@gmail.com>
2021-10-12 16:09:03 -0600

    fixed

--- scripts/pages/toolkit.js
+++ scripts/pages/toolkit.js
@@ -1,11 +1,10 @@
 export default function decorate($main) {
-  console.log("HITTTTTT")
     var toolkit = document.querySelector("body > main");
-    toolkit.classList.add("toolkit")
-    console.log("working")
-    toolkit.forEach(t => {
-      if( t.querySelector("p") ) {
-        t.classList.add("article-picture");
-      }
-    });
+    toolkit.classList.add("toolkit");
+    console.log("LOL", toolkit)
+    // toolkit.forEach(t => {
+    //   if( t.querySelector("p") ) {
+    //     t.classList.add("article-picture");
+    //   }
+    // });
 }
