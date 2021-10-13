5dc445941607e5d65d8b7a9ac24a5cc203ffa56d
Logan Kovash <logan.kovash@busyhuman.io>
2021-10-13 16:12:41 -0600

    changes to story

--- blocks/story/story.js
+++ blocks/story/story.js
@@ -7,12 +7,19 @@ export default function decorate($block) {
     const $stories = $block.querySelectorAll(':scope > div');
     for (const story of $stories) {
         story.classList.add("story");
-        
+        story.querySelector(".content > p:nth-child(3)").classList.add("openings");
+        story.querySelector(".content > p:nth-child(4)").classList.add("people");
+        story.querySelector(".content > p:nth-child(5)").classList.add("locations");
+        story.querySelector(".content > p:nth-child(6)").classList.add("zones");
     }
 
     var container = $wrap($element(".stories-stats"), 
     [
-      
+      document.querySelector("#think-about-design-differently"),
+      document.querySelector(".openings"),
+      document.querySelector(".people"),
+      document.querySelector(".locations"),
+      document.querySelector(".zones")
     ])
     mainContent.prepend(container);
 
