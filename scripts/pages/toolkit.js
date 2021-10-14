61753ea24e66938e83142d566f7fcaf7afed606b
wmichaelbischoff <wmichaelbischoff@gmail.com>
2021-10-13 18:12:40 -0600

    made toolkit look nice

--- scripts/pages/toolkit.js
+++ scripts/pages/toolkit.js
@@ -13,10 +13,52 @@ export default function decorate($main) {
     var remainder = $remainder(mainContent, ".toolkit-header");
     var pageContent = $wrap($element(".toolkit-page-content"), remainder)
     mainContent.append(pageContent);
+    document.querySelector("body > main > div > div > div.toolkit-page-content > p").classList.add('big-red-text')
+    var allArticles = document.querySelectorAll("body > main > div > div > div.toolkit-page-content > div")
+    allArticles.forEach(article => {
+      article.classList.add("js-scroll")
+      article.classList.add("fade-in")
+    })
 
-    // toolkit.forEach(t => {
-    //   if( t.querySelector("p") ) {
-    //     t.classList.add("article-picture");
-    //   }
-    // });
+    const scrollElements = document.querySelectorAll(".js-scroll");
+
+    const elementInView = (element, dividend = 1) => {
+      const elementTop = element.getBoundingClientRect().top;
+      
+      return (
+        elementTop <=
+        (window.innerHeight || document.documentElement.clientHeight) / dividend
+      );
+    };
+
+    const elementOutofView = (element) => {
+      const elementTop = element.getBoundingClientRect().top;
+
+      return (
+        elementTop > (window.innerHeight || document.documentElement.clientHeight)
+      );
+    };
+
+    const displayScrollElement = (element) => {
+      element.classList.add("scrolled");
+    };
+    const hideScrollElement = (element) => {
+      element.classList.remove("scrolled");
+    };
+
+    const handleScrollAnimation = () => {
+      console.log("SCROLLING")
+      scrollElements.forEach((element) => {
+        if (elementInView(element, 1.35)) {
+          displayScrollElement(element);
+        } else if (elementOutofView(element)) {
+          hideScrollElement(element)
+        }
+      })
+    }
+    displayScrollElement(document.querySelector("body > main > div > div > div.toolkit-page-content > div:nth-child(2)"));
+
+    window.addEventListener("scroll", () => { 
+      handleScrollAnimation();
+    });
 }
