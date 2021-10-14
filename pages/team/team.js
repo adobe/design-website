6d925a5aa77b8e5086a30f623b9f6f5679c6b71b
Connor O'Leary <connorroleary@gmail.com>
2021-10-14 11:21:38 -0600

    -Added slash to Think-differently -Made changed font-weight to be move like design doc

--- pages/team/team.js
+++ pages/team/team.js
@@ -140,6 +140,9 @@ export default function decorate($main) {
         thinkAboutBody.append(element)
       }
     })
+
+    thinkAboutBody.append($element(".think-differently-slash-1"))
+    thinkAboutBody.append($element(".think-differently-slash-2"))
     thinkAboutDiv.append(thinkAboutBody)
     endDiv.append(resourcesDiv)
     endDiv.append(thinkAboutDiv)
