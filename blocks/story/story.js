e246b53671265eb821a7aee1464c2a9ebd4c5eb1
Logan Kovash <logan.kovash@busyhuman.io>
2021-10-14 09:56:22 -0600

    changes to story files

--- blocks/story/story.js
+++ blocks/story/story.js
@@ -2,26 +2,20 @@ import {$wrap, $element} from "../../scripts/helpers.js";
 
 export default function decorate($block) {
 
-    var mainContent = $block.querySelector("body > main > div > div.content");
+    var mainContent = $block.querySelector(".section-wrapper > div");
 
-    const $stories = $block.querySelectorAll(':scope > div');
-    for (const story of $stories) {
-        story.classList.add("story");
-        story.querySelector(".content > p:nth-child(3)").classList.add("openings");
-        story.querySelector(".content > p:nth-child(4)").classList.add("people");
-        story.querySelector(".content > p:nth-child(5)").classList.add("locations");
-        story.querySelector(".content > p:nth-child(6)").classList.add("zones");
+    document.querySelector("body > main > div > div > div.story.block > div:nth-child(2) > div").classList.add("story-text");
+        // story.querySelector(".content > p:nth-child(4)").classList.add("people");
+        // story.querySelector(".content > p:nth-child(5)").classList.add("locations");
+        // story.querySelector(".content > p:nth-child(6)").classList.add("zones");
     }
 
-    var container = $wrap($element(".stories-stats"), 
-    [
-      document.querySelector("#think-about-design-differently"),
-      document.querySelector(".openings"),
-      document.querySelector(".people"),
-      document.querySelector(".locations"),
-      document.querySelector(".zones")
-    ])
-    mainContent.prepend(container);
-
-    
-}
+    // var container = $wrap($element(".stories-stats"), 
+    // [
+    //   document.querySelector("#think-about-design-differently"),
+    //   document.querySelector(".openings"),
+    //   document.querySelector(".people"),
+    //   document.querySelector(".locations"),
+    //   document.querySelector(".zones")
+    // ])
+    // mainContent.prepend(container);
