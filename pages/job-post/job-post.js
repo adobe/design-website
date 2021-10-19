c73250b96b836c261f5fcdb44b1ba3b15700c5c6
ShandelleLeigh <shandellejensen@gmail.com>
2021-10-19 15:01:40 -0600

    Progress on job-post blocks, pre-fragments

--- pages/job-post/job-post.js
+++ pages/job-post/job-post.js
@@ -11,6 +11,7 @@ import {
   getMetadata,
 } from "../../scripts/helpers.js";
 import makeHeaderBlock from "../../blocks/job-posting-blocks/job-post-header.js";
+// import makeAboutBlock from "../../blocks/job-posting-blocks/about-adobe-design.js";
 
 const bkg_grey_lt = '#E8E8E8',
       text_dark   = '#3E3E3E';
@@ -28,70 +29,57 @@ export default function decorate($page) {
   // document.querySelector("div.section-wrapper > div").classList.add("post-text");
 
 
-  let postContainer  = document.querySelector("main > div")
+  let postContainer  = document.querySelector("main > div");
   postContainer.classList.add("post-container");
-  let postText  = document.querySelector(".post-container > div")
+  let postText  = document.querySelector(".post-container > div");
   postText.classList.add("post-text");
-  let suggestedArticles = $element("div.suggested-articles")
-  let leftBlock = $element("div.left-block",[$element("div.spacer"),suggestedArticles])
-  postContainer.prepend(leftBlock)
-  // postText.classList.add("post-text");
-  const $infoContainer = document.createElement("div").classList.add("job-info-container")
-  // $infoContainer.append(document.querySelector("div.post-text"))
+  let suggestedArticles = $element("div.suggested-articles");
+  let leftBlock = $element("div.left-block",[$element("div.spacer"),suggestedArticles]);
+  postContainer.prepend(leftBlock);
+  const $infoContainer = document.createElement("div").classList.add("job-info-container");
 
-
-  // let $jobInfoContainer = $element("div.job-info-container", document.querySelector("body.job-post div.section-wrapper > div"))
-  //   document.querySelector("body.job-post div.section-wrapper > div").classList.add("job-info-container");
-  // let $jobPost =  $element("div.job-post", $jobInfoContainer)
-
-
-
-
-  /* Dark navy color: */
-    // Background.setColor( '#0B1C40' );
-  /* light grey color: */
-  // getMetadata('color')
 
   makeHeaderBlock($page);
   let title_h1 = document.querySelector("div.post-text > h1");
   if(title_h1) {
     title_h1.remove()
   };
-  let infoContainer = $wrap(postText, $element("div.job-info-container"))
-
-    /* Red color: */
-    // Background.setColor( '#E91D25' )
-
-
-  // let postText = document.querySelector(".job-info-container")
-  // document.querySelector(".header-block")
+  let infoContainer = $wrap(postText, $element("div.job-info-container"));
 
   // let postText= document.querySelector(".job-info-container").lastChild;
   // postText.classList.add("post-text");
   // document.querySelector("div.post-text")
-  let leftSpacer = $element("div.left-spacer")
-
-  // const $aboutContainer = $element('div.about-container')
+  let leftSpacer = $element("div.left-spacer");
 
   // postText.querySelectorAll('h6').forEach((tag) => {
-  //   tag.removeAttribute('id')
-  //   tag.classList.add('header-6')
-  //   // tag.appendTo($aboutContainer)
-  //   console.log( " TAG ", tag, "\n type: ", typeof tag)
-  // })
-  let buttonFunction = () => {console.log(" Clicked Apply Now Button")}
-  const $button_apply_now = addButton("Apply Now", buttonFunction, ['unfilled'], text_dark)
-  // $element("div.left", [$jobTitle, $button_apply_now]),
+    //   tag.removeAttribute('id')
+    //   tag.classList.add('header-6')
+    //   // tag.appendTo($aboutContainer)
+    //   console.log( " TAG ", tag, "\n type: ", typeof tag)
+    // })
+    let buttonFunction = () => {console.log(" Clicked Apply Now Button")};
+    const $button_apply_now = addButton("Apply Now", buttonFunction, ['unfilled'], text_dark);
+    // $element("div.left", [$jobTitle, $button_apply_now]),
 
-  postText.append($button_apply_now)
+    postText.append($button_apply_now);
 
 
-  // const $about = $element('div.about-adobe-design', $aboutContainer)
+    //--------------------------------------//
+    //----- About Adobe Design Element -----//
+    //--------------------------------------//
+    const aboutElm = $element('div.about-adobe-design')
+    aboutElm.innerHTML = '<div class="about-container"><h2 class="about-header">About Adobe Design</h2> <p class="about-text"> Adobe Design creates tools that amplify the world’s ability to create and communicate. We’re a global team of designers, researchers, prototypers, content strategists, program managers, and more who work across Adobe’s three product lines: Creative Cloud, Document Cloud, and Experience Cloud. </p>';
+    document.querySelector("main").append(aboutElm)
+    //--------------------------------------//
+    //----- Equal Opportunities Policy -----//
+    //--------------------------------------//
+    const eqOpPolicy = $element('div.eq-op-policy-stmnt')
+    eqOpPolicy.innerHTML = '<div class="container"> <p class="eq-op">Adobe is an equal opportunity employer. We hire talented individuals, regardless of gender, race, ethnicity, ancestry, age, disability, sexual orientation, gender identity or expression, veteran status, cultural background or religious beliefs. We know that when our employees feel appreciated and included, they can be more creative, innovative and successful. This is what it means to be Adobe For All.</p><p class="eq-op">Adobe is an equal opportunity and affirmative action employer. We welcome and encourage diversity in the workplace regardless of gender, race or color, ethnicity or national origin, age, disability, religion, sexual orientation, gender identity or expression, veteran status, or any other characteristics protected by law.</p><p class="eq-op">When you join Adobe, you can look forward to collaborating with the most genuine people in the industry, working on projects with real purpose, and having immense pride in the products we create and the customers we support. You will also be surrounded by colleagues who are committed to helping each other grow through our unique Check-In approach where ongoing feedback flows freely.</p><p class="eq-op">Come create experiences that matter at a company that is recognized around the world and hear what our employees are saying about their career experiences on the Adobe Life blog.</p></div>';
+    document.querySelector("main").append(eqOpPolicy)
 
-  // $wrap($about, postText)
-  // $about.appendTo(postText)
 
-  console.log( " POST TEXT ", postText, "\n postText: ", postText)
+
+
 
 
   // //----------//
