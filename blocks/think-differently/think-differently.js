b2cd1803322257cce49aa4729a5496e460730af0
Connor O'Leary <connorroleary@gmail.com>
2021-10-21 17:06:26 -0600

    -Added view jobs link to teamcards and think-differently -updated styling on job and team pages to be more like design doc

--- blocks/think-differently/think-differently.js
+++ blocks/think-differently/think-differently.js
@@ -6,11 +6,13 @@ export default function decorate($block){
     const query = $block.querySelectorAll("div:first-child > div:first-child >*")
     
     query.forEach(element =>{
+      
         if(element.nodeName === "P")
-            element.innerHTML = element.innerHTML.replace(/[0-9+]+/g, '<span class="think-differently-number">$&</span>')
+            element.innerHTML = element.innerHTML.replace(/[0-9,+]+/, '<span class="think-differently-number">$&</span>')
     })
     thinkAboutContent.append($element(".think-differently-slash.slash-1"))
     thinkAboutContent.append($element(".think-differently-slash.slash-2"))
+    thinkAboutContent.append($element("a.view-jobs", { attr: { href: '/' } }, "VIEW OUR JOB OPENINGS"))
     
     $block.classList.add("js-scroll", "fade-in-right")
     document.querySelectorAll(".think-differently-content > *").forEach((article, index) => {
