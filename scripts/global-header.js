d8d3c1611b6622522cd4cbb968afb6013a2b254f
Logan Kovash <logan.kovash@busyhuman.io>
2021-10-28 15:22:18 -0600

    updates to header and carousel

--- scripts/global-header.js
+++ scripts/global-header.js
@@ -2,7 +2,8 @@ export default function decorate(win = window) {
     console.log(win.document.location.pathname)
     const blackHeaderPages = ['/jobs/']
     const useBlackHeader = blackHeaderPages.includes(win.document.location.pathname)
-    var headerImage = useBlackHeader?'/resources/adobe-design.png':'./media_1d3601e007f7c9b5cb83303a390ca0e7fec040eef.png';
+    var headerImage = useBlackHeader?'/resources/tag+adobe.svg':'/resources/tag+adobe.svg';
+    var headerImageMobile = useBlackHeader?'/resources/adobe-design.png':'./media_1d3601e007f7c9b5cb83303a390ca0e7fec040eef.png';
     var hamburgerMenu = useBlackHeader ? '/resources/black-hamburger.png' : '/resources/white-hamburger.png';
     var closeIcon = useBlackHeader ? '/resources/close-icon.png' : '/resources/close-icon-white.png';
     var colorClass = useBlackHeader?'black':'';
@@ -34,9 +35,7 @@ export default function decorate(win = window) {
                                     </a>              \
                                 </div>              \
                             </div>                \
-                            <div class="tagline ${colorClass}"> \
-                                <p>Stories from the team designing Creative Cloud, Document Cloud, and Experience Cloud.</p> \
-                            </div>                \
+                                            \
                             <nav class="${colorClass}"> \
                                 <a href="/team/">Team</a> \
                                 <a href="/stories/">Stories</a> \
@@ -51,8 +50,8 @@ export default function decorate(win = window) {
                                 <div class="logo">  \
                                     <a href="/">      \
                                         <picture>       \
-                                        <source media="(max-width: 400px)" srcset="${headerImage}?width=750&format=webply&optimize=medium"> \
-                                        <img src="${headerImage}?width=2000&format=webply&optimize=medium" alt="" loading="eager"> \
+                                        <source media="(max-width: 400px)" srcset="${headerImageMobile}?width=750&format=webply&optimize=medium"> \
+                                        <img src="${headerImageMobile}?width=2000&format=webply&optimize=medium" alt="" loading="eager"> \
                                         </picture>      \
                                     </a>              \
                                 </div>              \
