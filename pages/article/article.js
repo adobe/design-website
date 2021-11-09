57371368b6d78bf627e242ec702ff061ee709148
Jonathan Oaklyn <jonathan.revell.hopkins@gmail.com>
2021-11-09 14:50:01 -0700

    Fixing some errors in articles

--- pages/article/article.js
+++ pages/article/article.js
@@ -11,16 +11,24 @@ export default function decorate($main) {
       }
     });
     // moveHeaderContent();
-    $main.querySelector(':scope > div > div').classList.add('content');
-    $main.querySelector('.header.block > div:nth-child(2) > div:nth-child(2)').classList.add('art');
+    try {
+      $main.querySelector(':scope > div > div').classList.add('content');
+      $main.querySelector('.header.block > div:nth-child(2) > div:nth-child(2)').classList.add('art');
+    } catch(err) {
+      console.error(err);
+    }
     buildSimilarStories();
     buildAuthorBio();
 
-    var headerTag = document.createElement('a')
+    var headerTag = document.createElement('a');
     headerTag.classList.add('header-tag')
-    headerTag.innerText = '#LEADING DESIGN'
-    document.querySelector('#whiteboarding-101').before(headerTag)
+    headerTag.innerText = '#LEADING DESIGN';
 
+    try {
+      document.querySelector('.content > h1').before(headerTag);
+    } catch(err) {
+      console.error(err);
+    }
 }
 
 async function buildSimilarStories(){
@@ -109,8 +117,12 @@ async function buildAuthorBio() {
       $element(".not-found", `Author ${authorName} not found`),
     ]);
   }
-  
-  document.querySelector('.block.header').append($authorBlock)
+
+  try {
+    document.querySelector('.block.header').append($authorBlock);
+  } catch(err) {
+    console.error(err);
+  }
 }
 
 function moveHeaderContent() {
