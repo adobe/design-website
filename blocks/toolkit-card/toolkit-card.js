ffc627fb44557508eceb52275dd8afe7cff75a42
Connor O'Leary <connorroleary@gmail.com>
2021-11-12 11:59:12 -0700

    Created basic toolkit-card block

---
+++ blocks/toolkit-card/toolkit-card.js
@@ -0,0 +1,66 @@
+import {
+    convertToBackground,
+    $element,
+    $wrap,
+    decorateTagLink,
+    decorateDivisions,
+  } from '../../scripts/helpers.js';
+  
+  /**
+   * @param {HTMLElement} $block
+   */
+  export default function decorate($block) {
+  
+    // Get the properties and identify the blocks
+    const result = decorateDivisions($block, [
+      '.image',
+    ]);
+    const props = result.properties;
+  
+    /**
+       *  Text Constants:
+       *
+       * $text   : Text half of the card
+       * $hed    : Header    / Title of Card
+       * $dek    : Subheader / summary
+       */
+    const $text = $element('.text');
+  
+    const HED_TEXT_LIMIT = 50;
+    const DEK_TEXT_LIMIT = 75;
+    let hedText = props.hed;
+    let dekText = props.dek;
+    if(truncateText){
+      dekText = props.dek.length<DEK_TEXT_LIMIT ? props.dek:props.dek.substring(0,DEK_TEXT_LIMIT-3)+'...';
+      hedText = props.hed.length<HED_TEXT_LIMIT ? props.hed:props.hed.substring(0,HED_TEXT_LIMIT-3)+'...';
+    }
+    const $hed = $element('.hed', hedText);
+    const $dek = $element('.dek', dekText);
+  
+    $text.append($hed, $dek);
+  
+    /* Apply the properties to the block */
+    $block.style.backgroundColor = result.properties.background;
+    $block.style.color = result.properties.textcolor;
+  
+    /* ---------  - IMAGES - ---------  */
+    /**
+       * Remove image and place on proper side:
+       */
+    result['.image'].remove();
+  
+    //TODO: Add paths to the card generation and use that instead of assuming the path is the same as the header
+    let path = props.path
+    const toolkitLink = $element('a.stories-link', { attr: { href: path.replaceAll(' ', '-').toLowerCase() } })
+    result['.block-content'].append(toolkitLink)
+    toolkitLink.prepend($text)
+  
+    if (!result.properties['image-side'] || result.properties['image-side'] === 'left') {
+        toolkitLink.prepend(result['.image']);
+    } else {
+        toolkitLink.append(result['.image']);
+    }
+  
+    result['.image'].classList.add('image');
+    convertToBackground(result['.image'].querySelector('img'), result['.image']);
+  }
