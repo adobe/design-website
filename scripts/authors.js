3c39fc6b3c6fafc535c817694d9e4b9d5d813c99
Logan Kovash <logan.kovash@busyhuman.io>
2021-11-17 14:31:55 -0700

    changes to author bio

--- scripts/authors.js
+++ scripts/authors.js
@@ -10,7 +10,7 @@ export async function lookupAuthor(name) {
 
         return {
             bio,
-            name: 'John Doe',
+            name: name,
             title: 'Director of Examples',
             image: './media_16fd3a6e96f49f43ba7d3ced9ec935ed493b7a305.png?width=750&amp;format=webply&amp;optimize=medium'
         }
