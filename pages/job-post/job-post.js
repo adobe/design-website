e8ba24362117d1d57cf94cd67e5c8499b0a4e66f
ShandelleLeigh <shandellejensen@gmail.com>
2021-12-01 13:21:51 -0700

    Job-posting sticky block mostly finished.

--- pages/job-post/job-post.js
+++ pages/job-post/job-post.js
@@ -2,9 +2,15 @@ import addButton from "../../blocks/button/button.js";
 import {
     $addMiddleElm,
     $element,
+    getMetadata,
 } from "../../scripts/helpers.js";
 import { getJobsFragment } from "../../scripts/jobs-fragments.js";
 import makeSimilarOpportunitiesBlock from "../../blocks/job-posting-blocks/similar-opportunities.js";
+
+
+const bkg_grey_lt = '#E8E8E8';
+const text_dark   = '#3E3E3E';
+
 
 /**
  * @param {HTMLElement} $page
@@ -20,31 +26,78 @@ export default async function decorate($page) {
     let postText = document.querySelector(".post-container > div");
     postText.classList.add("post-text");
 
-    let stickyContainer = $element("div.sticky-container")
-    postContainer.prepend(stickyContainer)
+    /* Assemble "Apply Now" Button. It'll be used 3x */
+    /* Declare the function for "onClick" action */
 
+    let jobURL = getMetadata('job-title')
+    let buttonFunction = () => {
+        console.log(" Clicked Apply Now Button")
+    };
+    const $button_apply_now = addButton("Apply Now", buttonFunction, 'unfilled lt-bkg', text_dark);
+    // postBody.append($button_apply_now);
+    //-- START Job Position details subheader --//
+    const $dek              = $element(".dek",           getMetadata('dek'));
+    const $location         = $element("p.detail-value", getMetadata('location'));
+    const $positionType     = $element("p.detail-value", getMetadata('position-type'));
+    const $req_number       = $element("p.detail-value", getMetadata('req-number') || 'None Provided');
+    const $header_details   = $element("div.subhead-container", [
+        $element("div.details", [
+            $element("span.detail", [
+                $element("p.detail-label", "Location"),
+                $location,
+            ]),
+            $element("span.detail", [
+                $element("p.detail-label", "Position Type"),
+                $positionType,
+            ]),
+            $element("span.detail", [
+                $element("p.detail-label", "Req Number"), $req_number
+            ]),
+        ]),
+        // $dek
+    ]);
+    //-- End Job Position details subheader --//
+
+
+    //-- START Job Posting Body Text Block --//
+
+
+    //-- END   Job Posting Body Text Block --//
+
+
+    //-- START Sticky bits: --//
+    let stickyContainer = $element("div.sticky-container")
+    postContainer.prepend(stickyContainer);
+    stickyContainer.append($button_apply_now)
+    stickyContainer.append($header_details)
+
+    //-- END Sticky bits: --//
+
+
+    //-- Start Delete --//
+    let uselessBoxElm = $element("div.boxy", $element("p.stuff", "WOrds and stuff"))
+    body_job_post.prepend(uselessBoxElm);
+    //-- END Delete --//
 
     $addMiddleElm(
         document.querySelector("div.section-wrapper"),
-        ".post-container",
+        ".post-body",
         document.querySelector(".post-text")
     )
 
-    // $addMiddleElm(
-    //     document.querySelector(".post-container"),
-    //     "div.inner_post_contnr",
-    //     document.querySelector(".post-text")
-    // )
 
 
+
+
+
+
+
+    /* ------------------------------------------------------------ */
     /* Assemble "Equal Opportunities" + "About Adobe Design" blocks */
     buildJobBlockFragments();
     /* Assemble "Similar Opportunities" block */
     buildSimOpportunitiesBlock();
     document.querySelector("main").append($element("div.similarOpps-block"));
-
-
-
 }
 
 async function buildSimOpportunitiesBlock(){
