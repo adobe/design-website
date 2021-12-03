94c9b5f79cf170c5ae80f89bda3c1b31d10181ef
Connor O'Leary <connorroleary@gmail.com>
2021-12-02 17:08:32 -0700

    Lint fix in TD, Jobs, Teams. Partial fix in 70/30

--- blocks/think-differently/think-differently.js
+++ blocks/think-differently/think-differently.js
@@ -1,20 +1,20 @@
-import {$element, $scrollAnimation } from "../../scripts/helpers.js";
+import { $element, $scrollAnimation } from '../../scripts/helpers.js';
 
-export default function decorate($block){
-    const thinkAboutContent = $block.querySelector("div:first-child > div:first-child")
-    thinkAboutContent.classList.add("think-differently-content")
-    const query = $block.querySelectorAll("div:first-child > div:first-child >*")
+export default function decorate($block) {
+  const thinkAboutContent = $block.querySelector('div:first-child > div:first-child');
+  thinkAboutContent.classList.add('think-differently-content');
+  const query = $block.querySelectorAll('div:first-child > div:first-child >*');
 
-    query.forEach(element =>{
+  query.forEach((element) => {
+    if (element.nodeName === 'P') {
+      element.innerHTML = element.innerHTML.replace(/[0-9,+]+/, '<span class="think-differently-number">$&</span>');
+    }
+  });
+  thinkAboutContent.append($element('.think-differently-slash.slash-1'));
+  thinkAboutContent.append($element('.think-differently-slash.slash-2'));
+  thinkAboutContent.append($element('a.view-jobs', { attr: { href: '/jobs/', target: '_blank' } }, 'VIEW OUR JOB OPENINGS'));
 
-        if(element.nodeName === "P")
-            element.innerHTML = element.innerHTML.replace(/[0-9,+]+/, '<span class="think-differently-number">$&</span>')
-    })
-    thinkAboutContent.append($element(".think-differently-slash.slash-1"))
-    thinkAboutContent.append($element(".think-differently-slash.slash-2"))
-    thinkAboutContent.append($element("a.view-jobs", { attr: { href: '/jobs/', target: "_blank"} }, "VIEW OUR JOB OPENINGS"))
+  $block.classList.add('js-scroll', 'fade-in');
 
-    $block.classList.add("js-scroll", "fade-in")
-
-    $scrollAnimation();
+  $scrollAnimation();
 }
