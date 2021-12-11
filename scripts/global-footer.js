8010d255d30c289ad400a724f21c050b9c761e73
ShandelleLeigh <shandellejensen@gmail.com>
2021-12-10 19:45:08 -0700

    Make global-footer px perfect 4 all screen sizes

--- scripts/global-footer.js
+++ scripts/global-footer.js
@@ -21,15 +21,21 @@ export default function decorate(win = window) {
           <a href="/toolkit/inclusive-design/">Inclusive Design</a>                          \
         </nav>                                                                               \
         <nav class="foot last">                                                              \
-          <p>Copyright © 2021 Adobe. All rights reserved.</p>                                \
-          <p class="med">/</p>                                                               \
-          <a id="privacy" href="" target="_blank">Privacy</a>                                \
-          <p class="sm">/</p>                                                                \
-          <a id="tou" href="" target="_blank">Terms of Use</a>                               \
-          <p class="sm">/</p>                                                                \
-          <a id="cookies" href="" target="_blank">Cookies</a>                                \
-          <p class="med">/</p>                                                               \
-          <a id="dns-pinfo" href="" target="_blank">Do not sell my personal information</a>  \
+          <div class="break lg">                                                             \
+            <p>Copyright © 2021 Adobe. All rights reserved.</p>                              \
+            <p class="lg">/</p>                                                              \
+          </div>                                                                             \
+          <div class="break med">                                                             \
+            <a id="privacy" href="" target="_blank">Privacy</a>                              \
+            <p class="sm">/</p>                                                              \
+            <a id="tou" href="" target="_blank">Terms of Use</a>                             \
+            <p class="sm">/</p>                                                              \
+            <a id="cookies" href="" target="_blank">Cookies</a>                              \
+          </div>                                                                             \
+          <div class="break sm">                                                            \
+            <p class="med">/</p>                                                             \
+            <a id="dns-pinfo" href="" target="_blank">Do not sell my personal information</a>\
+          </div>                                                                             \
         </nav>                                                                               \
       </div>';
   }
