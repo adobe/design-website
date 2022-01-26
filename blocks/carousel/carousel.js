e864ad7242ef1e4d280de2c0bf69d74e34448b72
cjgammon <gammon@adobe.com>
2022-01-26 15:44:18 -0600

    start styling and layout of carousel

--- blocks/carousel/carousel.js
+++ blocks/carousel/carousel.js
@@ -6,12 +6,39 @@ export default async function decorate(block) {
   const stories = await lookupPages(pathnames);
   const ul = document.createElement('ul');
   ul.classList.add('carousel-slides');
+  ul.style.width = `${stories.length * 100}%`;
   stories.forEach((row, i) => {
+    console.log(row);
+
     const li = document.createElement('li');
     li.classList.add('carousel-slide');
-    li.innerHTML = `<h2><a href="${row.path}">${row.title}</h2>`;
-    li.append(createOptimizedPicture(row.image, row.title, !i));
+    li.style.left = `${i * 100}%`;
+
+    const rowContent = document.createElement('div');
+    rowContent.classList.add('carousel-slide-content');
+    rowContent.innerHTML = `<h2><a href="${row.path}">${row.title}</h2>`;
+    rowContent.append(createOptimizedPicture(row.image, row.title, !i));
+    li.append(rowContent);
+
     ul.append(li);
   });
   block.append(ul);
+
+  // nav buttons
+  const leftBtn = document.createElement('div');
+  leftBtn.classList.add('carousel-btn');
+  leftBtn.classList.add('carousel-btn-left');
+  block.appendChild(leftBtn);
+
+  const rightBtn = document.createElement('div');
+  rightBtn.classList.add('carousel-btn');
+  rightBtn.classList.add('carousel-btn-right');
+  block.appendChild(rightBtn);
+
+  rightBtn.addEventListener('click', () => {
+
+  });
+  leftBtn.addEventListener('click', () => {
+
+  });
 }
