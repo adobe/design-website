f638cfe883dba09201c593ccef108f11121c8afd
cjgammon <gammon@adobe.com>
2022-02-08 11:18:17 -0600

    working on scroll behavior

--- blocks/team-accordion/team-accordion.js
+++ blocks/team-accordion/team-accordion.js
@@ -17,47 +17,57 @@ class Accordion {
     this.container = document.body.querySelector('.cmp-accordion__group');
     this.cards = document.body.querySelectorAll('.cmp-accordion-card');
 
+    for (let i = 0; i < this.cards.length; i += 1) {
+      const card = this.cards[i];
+      card.addEventListener('mouseout', () => this.cardMouseOut(i));
+      card.addEventListener('mouseover', () => this.cardMouseOver(i));
+      card.addEventListener('click', () => this.cardClick(i));
+    }
+
+    this.resize();
+
+    window.addEventListener('wheel', (e) => this.handleMousewheel(e));
+    window.addEventListener('resize', () => this.resize());
+  }
+
+  resize() {
+    this.selected = -1;
+    this.desktopSize();
+  }
+
+  desktopSize() {
     let size = 0;
 
     for (let i = 0; i < this.cards.length; i += 1) {
       const card = this.cards[i];
       const rect = card.getBoundingClientRect();
-      console.log(rect);
       card.dataset.index = i;
       card.style.position = 'absolute';
       card.style.top = `${i * visibleCardArea}px`;
       if (i === this.cards.length - 1) {
         size += rect.height;
       }
-
-      card.addEventListener('mouseout', () => this.cardMouseOut(i));
-      card.addEventListener('mouseover', () => this.cardMouseOver(i));
-      card.addEventListener('click', () => this.cardClick(i));
     }
 
     size += visibleCardArea * (this.cards.length - 1);
     this.container.style.height = `${size}px`;
-
-    window.addEventListener('wheel', (e) => this.handleMousewheel(e));
   }
 
   handleMousewheel(e) {
-    if (this.selected > 0 || this.selected < this.cards.length - 1) {
-      console.log('stopp');
-      e.preventDefault();
-      e.stopPropagation();
-    }
-
     if (this.scrolling) {
       return false;
     }
 
     if (this.selected !== -1) {
-      if (e.deltaY > 0 && this.selected < this.cards.length - 1) {
-        this.cardClick(this.selected + 1);
+      if (e.deltaY > 0 && this.selected < this.cards.length) {
+        if (this.selected === this.cards.length - 1) {
+          this.cardClick(-1);
+        } else {
+          this.cardClick(this.selected + 1);
+        }
       } else if (e.deltaY < 0 && this.selected > -1) {
         if (this.selected === 0) {
-          this.selected = -1;
+          this.cardClick(-1);
         } else {
           this.cardClick(this.selected - 1);
         }
@@ -98,17 +108,37 @@ class Accordion {
   }
 
   cardClick(i) {
-    const { gsap } = window;
-
     console.log(i);
+
+    const { gsap } = window;
     this.selected = i;
 
-    const card = this.cards[i];
-    const rect = card.getBoundingClientRect();
-
-    const newCardHeight = rect.height - visibleCardArea - 1;
     const lastCardRect = this.cards[this.cards.length - 1].getBoundingClientRect();
     const lastCardHeight = lastCardRect.height;
+
+    if (i === -1) {
+      // close cards
+      for (let j = 0; j < this.cards.length; j += 1) {
+        gsap.killTweensOf(this.cards[j]);
+        gsap.to(this.cards[j], {
+          duration: 1,
+          y: 0,
+        });
+      }
+
+      // reset container height
+      gsap.killTweensOf(this.container);
+      gsap.to(this.container, {
+        duration: 1,
+        height: ((this.cards.length - 1) * visibleCardArea) + lastCardHeight,
+      });
+      return;
+    }
+
+    // open card
+    const card = this.cards[i];
+    const rect = card.getBoundingClientRect();
+    const newCardHeight = rect.height - visibleCardArea - 1;
 
     for (let j = 0; j < this.cards.length; j += 1) {
       gsap.killTweensOf(this.cards[j]);
@@ -125,6 +155,7 @@ class Accordion {
       }
     }
 
+    // adjust container height
     let newHeight = (visibleCardArea * (this.cards.length - 1)) + newCardHeight + lastCardHeight;
     if (i === this.cards.length - 1) {
       newHeight = (visibleCardArea * (this.cards.length - 1)) + lastCardHeight;
@@ -136,6 +167,7 @@ class Accordion {
       height: newHeight,
     });
 
+    // scroll window
     const { container } = this;
     const containerRect = container.getBoundingClientRect();
     const newTop = document.documentElement.scrollTop + containerRect.top + (i * visibleCardArea);
