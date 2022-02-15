adb26c900314df2760e6847e4c3e20195b92aeca
Patrick Fulton <pfulton@adobe.com>
2022-02-15 15:51:05 -0500

    wip: html now rendering correctly

--- blocks/opportunities/opportunities.js
+++ blocks/opportunities/opportunities.js
@@ -12,7 +12,32 @@ export default async function decorate(block) {
     else groupedJobs[department].jobs.push(job);
   });
 
-  console.log(groupedJobs);
+  const jobsContainer = document.createElement('div');
+  jobsContainer.classList.add('jobs-container');
+  block.append(jobsContainer);
 
-  // console.log(departmentNames);
+  Object.keys(groupedJobs).forEach((key) => {
+    const departmentJobs = groupedJobs[key].jobs;
+    // create the headings for each group
+    const heading = document.createElement('h4');
+    const transformedKey = key.split('-').map((s) => s.charAt(0).toUpperCase() + s.substring(1)).join(' ');
+    heading.innerText = transformedKey;
+    jobsContainer.append(heading);
+
+    // get the rest of the jobs data
+    departmentJobs.forEach((job) => {
+      const list = document.createElement('ul');
+      list.classList.add('jobs-ul');
+
+      const listItem = document.createElement('li');
+      listItem.classList.add('jobs-list-item');
+      listItem.innerHTML = `
+        <h5><a href="${job.path}">${job.title}</a></h5>
+        <p>Department: ${job.department}</p>
+        <p>Location: ${job.location}</p>
+      `;
+      jobsContainer.append(list);
+      list.append(listItem);
+    });
+  });
 }
