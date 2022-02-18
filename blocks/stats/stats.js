154e980c4aeb284c4afa9f975e3e96861581c088
cjgammon <gammon@adobe.com>
2022-02-18 13:35:08 -0600

    work on styling

--- blocks/stats/stats.js
+++ blocks/stats/stats.js
@@ -3,7 +3,21 @@ export default async function decorate(block) {
   statsComponent.classList.add('cmp-stats');
 
   const statItems = statsComponent.querySelectorAll(':scope > div');
-  statItems.forEach((item) => { item.classList.add('cmp-stats__item'); });
+
+  let i = 0;
+  statItems.forEach((item) => {
+    i += 1;
+    item.classList.add('cmp-stats__item');
+
+    if (i < statItems.length) {
+      const slash = document.createElement('div');
+      slash.classList.add('cmp-stats__slash');
+      slash.innerHTML = `<svg xmlns="http://www.w3.org/2000/svg" width="98.467" height="149.677" viewBox="0 0 98.467 149.677">
+        <line id="Line_321" data-name="Line 321" x1="97.63" y2="149.129" transform="translate(0.418 0.274)" fill="none" stroke="#fff" stroke-width="1"/>
+      </svg>`;
+      item.parentNode.insertBefore(slash, item.nextSibling);
+    }
+  });
 
   const classNames = ['cmp-stats__item-number', 'cmp-stats__item-description', 'cmp-stats__item-tag'];
   statItems.forEach((item) => {
