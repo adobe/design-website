4967d51c9f5c483305ac3812492c38c769867415
CJ Gammon <gammon@adobe.com>
2022-02-25 12:31:53 -0600

    feat: inclusive design detail page & toolkit landing polish (#77)
    Adds and styles the Inclusive Design detail page, and also polishes the Toolkit landing page.

--- blocks/cards/cards.js
+++ blocks/cards/cards.js
@@ -1,5 +1,5 @@
 import {
-  createOptimizedPicture, lookupPages,
+  createOptimizedPicture, lookupPages, getMetadata,
 } from '../../scripts/scripts.js';
 import colormap from '../../scripts/colormap.js';
 
@@ -10,6 +10,7 @@ function createCard(row) {
   const cardDescription = `${row.description ? `<p class="cmp-cards-card__description">${row.description}</p>` : ''}`;
   const cardBGColor = row.color !== '' ? row.color : '#fff';
   const textColor = colormap[cardBGColor];
+  const url = row.path;
 
   if (textColor === 'black') {
     card.classList.add('dark-text');
@@ -20,7 +21,7 @@ function createCard(row) {
   card.innerHTML = `
     <div class="cmp-cards-card__body">
       <h2 class="cmp-cards-card__title">
-        <a href="${row.path}">${row.title}</a>
+        <a href="${url}">${row.title}</a>
       </h2>
       ${cardDescription}
     </div>
@@ -32,6 +33,40 @@ function createCard(row) {
   return (card);
 }
 
+function decorateInclusiveDesignPage(block) {
+  const cardsParent = block.parentNode;
+  cardsParent.classList.add('cmp-inclusive__inner-wrap');
+  const pageTitleWrap = document.createElement('div');
+  pageTitleWrap.classList.add('cmp-inclusive__title-wrap');
+
+  const pageTitle = document.querySelector('h1');
+  const pageTitleClone = pageTitle.cloneNode(true);
+  pageTitleClone.classList.add('page-title');
+
+  pageTitleWrap.append(pageTitleClone);
+  pageTitle.remove();
+
+  const childrenToWrap = [...cardsParent.children];
+  cardsParent.innerHTML = '';
+  cardsParent.append(pageTitleWrap);
+  const containerBG = document.createElement('div');
+  containerBG.classList.add('cmp-inclusive__bg');
+  cardsParent.append(containerBG);
+  containerBG.append(...childrenToWrap);
+
+  const heroImage = containerBG.querySelector('p > picture');
+  heroImage.parentElement.classList.add('cmp-inclusive__hero-parent');
+
+  const secondaryHeadline = document.querySelector('.cmp-inclusive__hero-parent').nextElementSibling;
+  secondaryHeadline.classList.add('cmp-inclusive__headline--secondary');
+
+  const introParagraph = secondaryHeadline.nextElementSibling;
+  introParagraph.classList.add('cmp-inclusive__intro');
+
+  const tertiaryHeadline = block.previousSibling;
+  tertiaryHeadline.classList.add('cmp-inclusive__headline--tertiary');
+}
+
 export default async function decorate(block) {
   const pathnames = [...block.querySelectorAll('a')].map((a) => new URL(a.href).pathname);
   block.textContent = '';
@@ -39,6 +74,13 @@ export default async function decorate(block) {
   cards.forEach((row) => {
     block.append(createCard(row));
   });
-  const cardsContainerInner = document.querySelector('.cards-container').firstChild;
-  cardsContainerInner.classList.add('cmp-cards__inner-wrap');
+
+  const isInclusiveDesignPage = getMetadata('theme') === 'inclusive-design';
+
+  if (!isInclusiveDesignPage) {
+    const cardsContainerInner = document.querySelector('.cards-container').firstChild;
+    cardsContainerInner.classList.add('cmp-cards__inner-wrap');
+  } else {
+    decorateInclusiveDesignPage(block);
+  }
 }
---
+++ blocks/inclusive-card/inclusive-card.js
@@ -0,0 +1,8 @@
+export default async function decorate() {
+  const wrapper = document.querySelector('.inclusive-design .section-wrapper');
+  wrapper.classList.add('inclusive-card-container');
+  wrapper.firstChild.classList.add('inclusive-card-container__inner');
+
+  const container = document.querySelector('.people-container');
+  container.firstChild.classList.add('inclusive-card-container__inner');
+}
