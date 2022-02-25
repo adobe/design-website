baed0683fc7f64c2995c41d9541639e1265aecb3
Patrick Fulton <pfulton@adobe.com>
2022-02-25 14:34:19 -0500

    fix: never show undefined job departments

--- blocks/opportunities/opportunities.js
+++ blocks/opportunities/opportunities.js
@@ -19,6 +19,7 @@ export default async function decorate(block) {
 
   allJobs.forEach((job) => {
     const department = job.path.split('/')[3];
+    if (!department) return;
     if (!groupedJobs[department]) groupedJobs[department] = { jobs: [] };
     if (job.path.endsWith('/')) groupedJobs[department].title = job.title;
     else groupedJobs[department].jobs.push(job);
