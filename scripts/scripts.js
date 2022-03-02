b8efc7910334113396be077547df4b9486219a1c
Patrick Fulton <pfulton@adobe.com>
2022-03-02 16:13:18 -0500

    feat: add favicons
    Closes #61

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -615,7 +615,7 @@ async function loadLazy(doc) {
   loadFooter(doc.querySelector('footer'));
 
   loadCSS(`${window.hlx.codeBasePath}/styles/lazy-styles.css`);
-  addFavIcon(`${window.hlx.codeBasePath}/styles/favicon.svg`);
+  addFavIcon(`${window.hlx.codeBasePath}/icon.svg`);
 }
 
 /**
