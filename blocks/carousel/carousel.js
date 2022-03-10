9aa0b947f793b65bbea959975f190c32e4664798
cjgammon <gammon@adobe.com>
2022-03-10 14:23:58 -0600

    layout adjustments for carousel

--- blocks/carousel/carousel.js
+++ blocks/carousel/carousel.js
@@ -219,7 +219,7 @@ export default async function decorate(block) {
     const slideCopy = document.createElement('div');
     slideCopy.classList.add('carousel-slide-copy');
     slideCopy.innerHTML = `
-    <span class="cmp-stories-card__tag">${tag}</span>
+    <span class="cmp-stories-card__tag carousel-stories-card__tag">${tag}</span>
     <h2 class="carousel-stories-card__title"><a href="${row.path}">${row.title}</a></h2>
     <div class="cmp-stories-card__intro">${row.subtitle}</div>
     <div class="cmp-stories-card__author">${row.author}</div>
