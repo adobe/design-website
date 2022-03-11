a23ecf9afea4f34c7ee0f2d162b3d11811aab2f2
CJ Gammon <gammon@adobe.com>
2022-03-11 11:26:13 -0600

    Carousel Updates (#152)
    * adjustments to carousel style
    
    * adjustments based on joslyn feedback
    
    * fix margin
    
    * update carousel width
    
    * adjust layout of caoursel widht
    
    * adjust position of buttons
    
    * adjust position of buttons on mobile
    
    * minor margin tweak
    
    * layout adjustments for carousel

--- blocks/carousel/carousel.js
+++ blocks/carousel/carousel.js
@@ -10,7 +10,7 @@ class Carousel {
 
   animation;
 
-  slideDelay = 3.5;
+  slideDelay = 8.5;
 
   slideDuration = 1;
 
@@ -219,15 +219,18 @@ export default async function decorate(block) {
     const slideCopy = document.createElement('div');
     slideCopy.classList.add('carousel-slide-copy');
     slideCopy.innerHTML = `
-    <span class="cmp-stories-card__tag">${tag}</span>
-    <h2 class="cmp-stories-card__title"><a href="${row.path}">${row.title}</a></h2>
+    <span class="cmp-stories-card__tag carousel-stories-card__tag">${tag}</span>
+    <h2 class="carousel-stories-card__title"><a href="${row.path}">${row.title}</a></h2>
     <div class="cmp-stories-card__intro">${row.subtitle}</div>
     <div class="cmp-stories-card__author">${row.author}</div>
     <div>${row.authorTitle}</div>`;
 
     slideContent.append(slideCopy);
 
-    slideContent.append(createOptimizedPicture(row.image, row.title, !i));
+    const pictureHolder = document.createElement('div');
+    pictureHolder.classList.add('caoursel-picture-holder');
+    slideContent.append(pictureHolder);
+    pictureHolder.append(createOptimizedPicture(row.image, row.title, !i));
 
     slideContainer.append(slideContent);
     li.append(slideContainer);
