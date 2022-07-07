1df56bf1fc858c2b7e1eb166a3ec0eb6d9e5fa8c
zastrow <philip@zastrow.co>
2022-07-06 22:21:52 -0400

    feat: header quote

---
+++ blocks/quote-header/quote-header.js
@@ -0,0 +1,22 @@
+export default async function decorate(block) {
+  const quoteContainer = document.createElement('blockquote');
+  block.parentNode.insertBefore(quoteContainer, block);
+  quoteContainer.classList.add('quote-header');
+
+  const quoteContent = block.querySelector('div:first-child');
+  const quoteCitation = block.querySelector('div:last-child:not(:first-child)') ? block.querySelector('div:last-child:not(:first-child)') : null;
+
+  // Removes extra <div>
+  quoteContent.innerText = quoteContent.innerText; // eslint-disable-line no-self-assign
+
+  // Adds usable class
+  quoteContent.classList.add('quote-header__content');
+
+  if (quoteCitation) {
+    quoteCitation.innerText = quoteCitation.innerText; // eslint-disable-line no-self-assign
+    quoteCitation.classList.add('quote-header__citation');
+  }
+
+  quoteContainer.innerHTML = block.innerHTML;
+  block.remove();
+}
