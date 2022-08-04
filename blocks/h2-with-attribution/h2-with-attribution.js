f80c93517759ff8acf4ec293475130655710b350
zastrow <philip@zastrow.co>
2022-08-04 13:19:18 -0400

    refactor: change quote-header to h2-with-attribution

--- blocks/h2-with-attribution/h2-with-attribution.js
+++ blocks/h2-with-attribution/h2-with-attribution.js
@@ -1,22 +1,24 @@
 export default async function decorate(block) {
+  
   const quoteContainer = document.createElement('blockquote');
   block.parentNode.insertBefore(quoteContainer, block);
-  quoteContainer.classList.add('quote-header');
-
+  quoteContainer.classList.add('h2-with-attribution');
+  
   const quoteContent = block.querySelector('div:first-child');
   const quoteCitation = block.querySelector('div:last-child:not(:first-child)') ? block.querySelector('div:last-child:not(:first-child)') : null;
-
+  
   // Removes extra <div>
   quoteContent.innerText = quoteContent.innerText; // eslint-disable-line no-self-assign
-
+  
   // Adds usable class
   quoteContent.classList.add('quote-header__content');
-
+  
   if (quoteCitation) {
     quoteCitation.innerText = quoteCitation.innerText; // eslint-disable-line no-self-assign
     quoteCitation.classList.add('quote-header__citation');
   }
-
+  
   quoteContainer.innerHTML = block.innerHTML;
-  block.remove();
+  
+ 
 }
