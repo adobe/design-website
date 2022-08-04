e2e2623263ade2b1e7d5159c28412c50b329e9c5
Osmond Arnesto <osmond@heysparkbox.com>
2022-08-04 14:21:21 -0700

    feat: Replace `blockquote` with `h2`
    - Check if `a` included in content or citation, retain link if so

--- blocks/h2-with-attribution/h2-with-attribution.js
+++ blocks/h2-with-attribution/h2-with-attribution.js
@@ -1,24 +1,35 @@
-export default async function decorate(block) {
-  
-  const quoteContainer = document.createElement('blockquote');
-  block.parentNode.insertBefore(quoteContainer, block);
-  quoteContainer.classList.add('h2-with-attribution');
-  
-  const quoteContent = block.querySelector('div:first-child');
-  const quoteCitation = block.querySelector('div:last-child:not(:first-child)') ? block.querySelector('div:last-child:not(:first-child)') : null;
-  
-  // Removes extra <div>
-  quoteContent.innerText = quoteContent.innerText; // eslint-disable-line no-self-assign
-  
-  // Adds usable class
-  quoteContent.classList.add('quote-header__content');
-  
-  if (quoteCitation) {
-    quoteCitation.innerText = quoteCitation.innerText; // eslint-disable-line no-self-assign
-    quoteCitation.classList.add('quote-header__citation');
+function returnTHMLWithRetainedLink(node) {
+  let nodeHTML = node.innerText;
+  const nodeLink = node.querySelector('a');
+
+  if (nodeLink) {
+    // Capture anchor tag within innerHTML
+    const linkHTML = node.innerHTML.match(/<a href=.*>.*<\/a>/)[0];
+    // Capture link text within anchor tag
+    const linkText = linkHTML.match(/(?<=<a href=.*>).*(?=<\/a>)/)[0];
+    nodeHTML = nodeHTML.replace(linkText, linkHTML);
   }
-  
-  quoteContainer.innerHTML = block.innerHTML;
-  
- 
+
+  return nodeHTML;
+}
+
+export default async function decorate(block) {
+  const attributedHeadingContainer = document.createElement('h2');
+  block.parentNode.insertBefore(attributedHeadingContainer, block);
+  attributedHeadingContainer.classList.add('h2-with-attribution');
+
+  const headingContent = block.querySelector('div:first-child');
+  const headingCitation = block.querySelector('div:last-child:not(:first-child)') ? block.querySelector('div:last-child:not(:first-child)') : null;
+
+  const contentHTML = returnTHMLWithRetainedLink(headingContent);
+  headingContent.innerHTML = contentHTML;
+
+  if (headingCitation) {
+    const citationHTML = returnTHMLWithRetainedLink(headingCitation);
+    headingCitation.innerHTML = citationHTML;
+  }
+
+  attributedHeadingContainer.innerHTML = block.innerHTML;
+  // Removes extra <div>
+  block.remove();
 }
