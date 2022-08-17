5ab0619c7f1c140d96ebe9bc0537e12492f91668
Osmond Arnesto <osmond@heysparkbox.com>
2022-08-17 12:00:03 -0700

    feat: Header, button copy, button link in CTA editable

--- blocks/cta/cta.js
+++ blocks/cta/cta.js
@@ -22,6 +22,11 @@ export default async function decorate(block) {
   makeCTA.className = 'cmp-cta';
   let ctaJobs = '';
 
+  // Capture header and button copy
+  const headerCopy = block.querySelector('.cta > div:nth-child(1) > div + div').innerText;
+  const buttonCopy = block.querySelector('.cta > div:nth-child(2) > div + div').innerText;
+  const buttonPath = block.querySelector('.cta > div:nth-child(3) > div + div').innerText;
+
   // Check for jobs and generate HTML
   const links = [...block.querySelectorAll('a')];
   const pathnames = links.map((a) => new URL(a.href).pathname);
@@ -32,9 +37,9 @@ export default async function decorate(block) {
 
   // Construct CTA
   makeCTA.innerHTML = `
-    <h2 class="cmp-cta__content">Design your career at Adobe.</h2>
+    <h2 class="cmp-cta__content">${headerCopy}</h2>
     ${ctaJobs}
-    <a class="cmp-cta__btn" href="/jobs">View all jobs</a>
+    <a class="cmp-cta__btn" href=${buttonPath}>${buttonCopy}</a>
   `;
 
   // Insert above footer on page and remove original block
