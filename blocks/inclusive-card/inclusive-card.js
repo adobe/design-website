948d9705fc2b2807d66a157e9318382ef84b292c
Osmond Arnesto <osmond@heysparkbox.com>
2022-11-11 15:28:14 -0800

    feat: test deletion of unneeded blocks

--- blocks/inclusive-card/inclusive-card.js
+++
@@ -1,8 +0,0 @@
-export default async function decorate() {
-  const wrapper = document.querySelector('.inclusive-design .section-wrapper');
-  wrapper.classList.add('inclusive-card-container');
-  wrapper.firstChild.classList.add('inclusive-card-container__inner');
-
-  const container = document.querySelector('.people-container');
-  container.firstChild.classList.add('inclusive-card-container__inner');
-}
--- blocks/think-differently/think-differently.js
+++
@@ -1,7 +0,0 @@
-export default async function decorate(block) {
-  block.classList.add('cmp-stats');
-  const statsInnerWrap = block.firstChild;
-  statsInnerWrap.classList.add('cmp-stats__inner-wrap');
-
-  statsInnerWrap.querySelectorAll('h2').forEach((el) => el.classList.add('cmp-stats__heading'));
-}
